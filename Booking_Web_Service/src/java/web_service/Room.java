cf2246b971c58f571e7306c15fde2301f8c5dc85
Jeremiah Martinez <69489707+JeremiahCM@users.noreply.github.com>
2021-05-15 17:59:01 +1200

    Created student and room Beans
    Made Java Beans for Students and Rooms

--- Booking_Web_Service/src/java/web_service/Room.java
+++ Booking_Web_Service/src/java/web_service/Room.java
@@ -1,14 +1,66 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * JPA Entity class that represents a Room, as a Java Bean
+ * Entity has attribute for a room ID, room name and romm location
+ * Implemented get and set methods to acccess and manage these attributes
+ * @author Jeremiah Martinez: 18027693 | ?: ?
+ * 
+ * The files have used code learned from several stackoverflow threads, Telusko youtube channel and reused
+ * from lab exercises, few bits of the following is adapted not copy and pasted, except for lab code we have used
+*/
 package web_service;
 
-/**
- *
- * @author ItsKi
- */
-public class Room {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Define class Task to be used for dgn1399_etasks table in database
+@Entity
+@Table(name = "dgn1399_rooms")
+public class Room implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int r_id;
+    @Column(name = "room_name")
+    private String roomName;
+    @Column(name = "room_location")
+    private String roomLocation;
     
+    public Room()
+    {
+    }
+    
+    public void setRoom(String roomName)
+    {
+        this.roomName = roomName;
+    }
+    
+    public String getRoom()
+    {
+        return this.roomName;
+    }
+    
+    public void setLocation(String roomLocation)
+    {
+        this.roomLocation = roomLocation;
+    }
+    
+    public String getLocation()
+    {
+        return this.roomLocation;
+    }
+    
+    public void setRoomID(int r_id)
+    {
+        this.r_id = r_id;
+    }
+    
+    public int getRoomID()
+    {
+        return this.r_id;
+    }
 }
--- Booking_Web_Service/src/java/web_service/Student.java
+++ Booking_Web_Service/src/java/web_service/Student.java
@@ -1,14 +1,89 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * JPA Entity class that represents a Student, as a Java Bean
+ * Entity has attribute for student ID, password, first name and last name
+ * Implemented get and set methods to acccess and manage these attributes
+ * @author Jeremiah Martinez: 18027693 | ?: ?
+ * 
+ * The files have used code learned from several stackoverflow threads, Telusko youtube channel and reused
+ * from lab exercises, few bits of the following is adapted not copy and pasted, except for lab code we have used
+*/
 package web_service;
 
-/**
- *
- * @author ItsKi
- */
-public class Student {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Define class Entity to be used for dgn1399_students table in database
+@Entity
+@Table(name = "dgn1399_students")
+public class Student implements Serializable
+{
+    /*Entity will have an automatically generated ID, incremented one above the
+    previous student ID generated*/
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int s_id;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "first_name")
+    private String fName;
+    @Column(name = "last_name")
+    private String lName;
+
+    public Student()
+    {
+    }
+
+    //Setter for first name
+    public void setFname(String fName)
+    {
+        this.fName = fName;
+    }
+
+    //Getter for first name
+    public String getFname()
+    {
+        return fName;
+    }
     
+    //Setter for password
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    
+    //Getter for password
+    public String getPassword()
+    {
+        return password;
+    }
+
+    //Setter for last name
+    public void setLname(String lName)
+    {
+        this.lName = lName;
+    }
+
+    //Getter for last name
+    public String getLname()
+    {
+        return lName;
+    }
+
+    //Setter for student ID
+    public void setStuID(int e_id)
+    {
+        this.s_id = e_id;
+    }
+
+    //Getter for student ID
+    public int getStuID()
+    {
+        return s_id;
+    }
 }
